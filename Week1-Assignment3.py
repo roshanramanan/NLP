a8d970a9a532369c56f96fba83b6293b96fdddf6
RRemanan <roshan.ramanan@gmail.com>
2019-03-23 15:54:58 -0500

    Regular expression

--- Week1-Assignment3.py
+++ Week1-Assignment3.py
@@ -1,18 +1,45 @@
-from cgitb import html
-from urllib import request
-
 import nltk
-from bs4 import BeautifulSoup
 
-url = "http://www.gutenberg.org/files/2554/2554-0.txt"
-response = request.urlopen(url)
-raw = response.read().decode("utf-8-sig")
-raw.find("PART I")
-raw.rfind("End of Project")
-raw = raw[5335:1157809]
-tokens = nltk.word_tokenize(raw)
-text = nltk.Text(tokens)
-text.collocations()
+print("Regular Expression Validator")
+print("**************************************************************************")
+print("[a-zA-Z]+")
+print("Alphabet at least and more than at one time")
+print("Example : ")
+nltk.re_show(r'[a-zA-Z]+', 'a abc aBcd ABcd ABCD a1234 12A34 aB1234')
 
-raw = BeautifulSoup(html, "lxml").get_text()
-print(raw)
+print("**************************************************************************")
+print("[A-Z][a-z]*")
+print("Start with upper case after that lower case is coming but lower cases can be omitted")
+print("Example : ")
+nltk.re_show(r'[A-Z][a-z]*', 'a abc aBcd ABcd ABCD a1234 12A34 aB1234')
+
+print("**************************************************************************")
+print("p[aeiou]{,2}t")
+print("start with ‘p’ and end with ‘t’ between them 0 to 2 vowels(aeiou) can be inserted")
+print("Example : ")
+nltk.re_show(r'p[aeiou]{,2}t', 'pit pet peat pool good puuut pt')
+
+print("**************************************************************************")
+print("\d+(\.\d+)?")
+print("‘\d’ means numbers, therefore numbers with decimals and under decimal point is optional")
+print("Example : ")
+nltk.re_show(r'\d+(\.\d+)?', '0 0.2 12.0003 -4 -5.2 5,000')
+
+print("**************************************************************************")
+print("a([^aeiou][aeiou][^aeiou])*")
+print("- Exactly once: All of:")
+print("-1. Exactly once: Match 'a'")
+print("- 2.Zero or more times: Subexpression: Exactly once: All of: ")
+print("1. Exactly once: Match '[^aeiou]'")
+print(" 2. Exactly once: Match '[aeiou]'")
+print("3. Exactly once: Match '[^aeiou]'")
+print("Example : ")
+nltk.re_show(r'a([^aeiou][aeiou][^aeiou])*', 'pit pet peat pool good puuut pt')
+
+print("**************************************************************************")
+print("\w+|[^\w\s]+")
+print("- Exactly once: One of:")
+print("-1. One or more times: Match '\w'")
+print("-2. One or more times: Match '[^\w\s]'")
+print("Example : ")
+nltk.re_show(r'\w+|[^\w\s]+', 'roshan.ramanan@gmail.com')
