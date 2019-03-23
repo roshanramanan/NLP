6f4756e9119ef2cf65b5c9c806e3b2405c0753f0
RRemanan <roshan.ramanan@gmail.com>
2019-03-23 15:09:44 -0500

    Assignment 2

--- Week1-Assignment2.py
+++ Week1-Assignment2.py
@@ -1,9 +1,18 @@
-import nltk
-from nltk.corpus import brown, stopwords
-from nltk.book import *
-from nltk.probability import FreqDist
-from collections import Counter
+from cgitb import html
+from urllib import request
 
-print("**************************************************************")
-print("Chapter1 Question 1")
-print("**************************************************************")
+import nltk
+from bs4 import BeautifulSoup
+
+url = "http://www.gutenberg.org/files/2554/2554-0.txt"
+response = request.urlopen(url)
+raw = response.read().decode("utf-8-sig")
+raw.find("PART I")
+raw.rfind("End of Project")
+raw = raw[5335:1157809]
+tokens = nltk.word_tokenize(raw)
+text = nltk.Text(tokens)
+text.collocations()
+
+raw = BeautifulSoup(html, "lxml").get_text()
+print(raw)
